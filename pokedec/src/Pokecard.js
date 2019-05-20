f9cfec9976fa99fba3cb84765a629e0ee3aff800
CodeInDna <ykarelia@gmail.com>
2019-05-20 09:05:14 +0530

    Initail Commit

---
+++ pokedec/src/Pokecard.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Pokecard extends Component{
+	render(){
+		return(
+			<div></div>
+		)
+	}
+}
+
+export default Pokecard;
---
+++ pokedec/src/Pokedec.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Pokedec extends Component{
+  render(){
+    return(
+      <div>Pokemon</div>
+    )
+  }
+}
+
+export default Pokedec;
