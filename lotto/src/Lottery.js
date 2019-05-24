94958cc22cb38d7ad15cfd1c84ccdc9c553d1e09
CodeInDna <ykarelia@gmail.com>
2019-05-24 10:12:37 +0530

    Lotto Logic + Styling: Done!

--- lotto/src/Lottery.js
+++ lotto/src/Lottery.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import LotteryBall from './LotteryBall';
-// import './App.css';
+import './Lottery.css';
+import lottery from './lottery.png';
+
 
 class Lottery extends Component{
   static defaultProps = {
@@ -10,22 +12,28 @@ class Lottery extends Component{
   }
   constructor(props){
     super(props);
+    // making array of empty numBalls(6: default)
     this.state={nums:Array.from({length: this.props.numBalls})}
   }
   handleClick = () => {
-     this.setState(currState => ({
-      nums: currState.nums.map(res => Math.floor(Math.random() * this.props.maxNum) + 1)
-    }))
+      // updating the state with random numbers
+      this.setState(currState => ({
+        nums: currState.nums.map(res => Math.floor(Math.random() * this.props.maxNum) + 1)
+      }))
   }
   render(){
-    const {title, numBalls, maxNum} = this.props;
+    const {title} = this.props;
     return(
       <section className="Lottery">
-        <h1>{title}</h1>
+        <h1>
+          <img alt="lottery" src={lottery} 
+          style={{width: 70, height: 70, verticalAlign:'middle'}}/> 
+            {title}
+        </h1>
         <div>
-          {this.state.nums.map(res => <LotteryBall nums={res}/>)}
+          {this.state.nums.map((res, i) => <LotteryBall nums={res} key={i}/>)}
         </div>
-        <button onClick={this.handleClick}>Generate</button>
+        <button onClick={this.handleClick} className="Lottery-button">Generate</button>
       </section>
     )
   }
